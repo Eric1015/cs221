5914c95e3bdfd3bdc4101aa8e35dd75f1a27c935
love1 <love1dbsb77@hotmail.com>
2018-03-19 23:50:53 -0700

    stats.cpp is added

---
+++ PA3/pa3/stats.cpp
@@ -0,0 +1,22 @@
+#include "stats.h"
+#include "cs221util/PNG.h"
+#include "cs221util/RGBAPixel.h"
+#include <utility>
+#include <vector>
+using namespace std;
+using namespace cs221util;
+
+long getSum(char channel, pair<int, int> ul, pair<int, int> lr) {
+	if (channel == 'r') {
+
+	}
+	else if (channel == 'g') {
+
+	}
+	else if (channel == 'b') {
+
+	}
+	else {
+		
+	}
+}
