5a67d0021e789633edd31ebe8d66575796de26d8
love1 <love1dbsb77@hotmail.com>
2018-03-21 12:06:56 -0700

    buildTree and twoDtree constructor done

--- PA3/pa3/twoDtree.cpp
+++ PA3/pa3/twoDtree.cpp
@@ -36,12 +36,67 @@ twoDtree & twoDtree::operator=(const twoDtree & rhs){
 
 twoDtree::twoDtree(PNG & imIn){ 
 	stats s = stats(imIn); 
+	pair<int, int> start;
+	pair<int, int> end;
+	start.first = 0;
+	start.second = 0;
+	end.first = imIn.width();
+	end.second = imIn.height();
+	root = buildTree(s, start, end);
 }
 
 twoDtree::Node * twoDtree::buildTree(stats & s, pair<int,int> ul, pair<int,int> lr) {
 	if (ul.first != lr.first || ul.second != lr.second) {
-		long smallest;
+		pair<int, int> currUL;
+		pair<int, int> currLR;
+		currUL.first = ul.first;
+		currUL.second = ul.second;
+		currLR.first = ul.first;
+		currLR.second = lr.second;
+		long smallest = s.getScore(currUL, currLR);
+		pair<int, int> smallestPair;
+		smallestPair.first = currLR.first;
+		smallestPair.second = currLR.second;
+		for (unsigned i = ul.first + 1; i < lr.first; i++) {
+			currLR.first = i;
+			long temp = s.getScore(currUL, currLR);
+			if (temp < smallest) {
+				smallest = temp;
+				smallestPair.first = currLR.first;
+				smallestPair.second = currLR.second;
+			}
+		}
+		for (unsigned i = ul.second; i < lr.second; i++) {
+			currLR.second = i;
+			long temp = s.getScore(currUL, currLR);
+			if (temp < smallest) {
+				smallest = temp;
+				smallestPair.first = currLR.first;
+				smallestPair.second = currLR.second;
+			}
+		}
+		RGBAPixel ave = s.getAvg(ul, lr);
+		Node child = Node(ul, lr, ave);
+		Node* leftChild = buildTree(s, ul, smallestPair);
+		pair<int, int> next;
+		if (smallestPair.first == lr.first) {
+			next.first = ul.first;
+			next.second = leftChild->lowRight.second + 1;
+		}
+		else if (smallestPair.second == ul.second) {
+			next.first = leftChild->lowRight.first + 1;
+			next.second = ul.second;
+		}
+		Node* rightChild = buildTree(s, next, lr);
+		child.left = leftChild;
+		child.right = rightChild;
 
+		return &child;
+	}
+	else {
+		RGBAPixel ave = s.getAvg(ul, lr);
+		Node node = Node(ul, lr, ave);
+		return &node;
 	}
 }
 
